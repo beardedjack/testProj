a94b50aff63ffecb516449b1daa3be49533c51a3
kondrashov <evgen.kondrashoff@gmail.com>
2016-06-16 15:38:54 +0400

    before css

--- testSite/models.py
+++ testSite/models.py
@@ -3,20 +3,22 @@ from django.db import models
 # Create your models here.
 
 class Category(models.Model):
-    name = models.CharField(max_length = 30, unique = True)
-    description = models.TextField()
+    name = models.CharField(verbose_name="Наименование", max_length = 30, unique = True)
+    description = models.TextField(verbose_name="Описание")
 
     def __str__(self):
         return self.name
 
-
+    class Meta:
+        verbose_name = "Категория"
+        verbose_name_plural = "Категории"
 
 class Good(models.Model):
-    name = models.CharField(max_length = 5, unique = True)
-    description = models.TextField()
-    in_stock = models.BooleanField(default = True, db_index = True)
+    name = models.CharField(verbose_name="Наименование", max_length = 50, unique = True)
+    description = models.TextField(verbose_name="Описание")
+    in_stock = models.BooleanField(verbose_name="В наличии", default = True, db_index = True)
     category = models.ForeignKey(Category)
-    price = models.DecimalField(decimal_places = 2, max_digits = 8, default = 0)
+    price = models.DecimalField(verbose_name="Стоимость", decimal_places = 2, max_digits = 8, default = 0)
 
     def get_is_stock(self):
         if self.in_stock:
@@ -25,9 +27,14 @@ class Good(models.Model):
             return ""
 
     def __str__(self):
-        s = self.name
+        s = self.name + " (" + Category.objects.get(pk = self.category.pk).name + ")"
+
         if not self.in_stock:
-            s = s + " (нет в наличии)"
+            s = s + " (нет в наличии) "
+
+        if not self.price > 0:
+            s = s + " (нет стоимости)"
+
         return s
 
     class Meta:
--- testSite/views.py
+++ testSite/views.py
@@ -5,24 +5,41 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from testSite.models import Category, Good
 
+
+# def index(request, id):
+#     if id == None:
+#         cat = Category.objects.first()
+#     else:
+#         cat = Category.objects.get(pk = id)
+#     goods = Good.objects.filter(category = cat).order_by("name")
+#     s = "Категория: " + cat.name + "<br><br>"
+#     for good in goods:
+#         s = s + "(" + str(good.pk) + ") " + good.name + "<br>"
+#     return HttpResponse(s)
+
 def index(request, id):
+    cats = Category.objects.all().order_by("name")
     if id == None:
         cat = Category.objects.first()
     else:
         cat = Category.objects.get(pk = id)
     goods = Good.objects.filter(category = cat).order_by("name")
-    s = "Категория: " + cat.name + "<br><br>"
-    for good in goods:
-        s = s + "(" + str(good.pk) + ") " + good.name + "<br>"
-    return HttpResponse(s)
+    return render(request, "index.html", {"category": cat, "cats": cats, "goods": goods})
+
+# def good(request, id):
+#     if id == None:
+#         s = "Не выбран id товара"
+#     else:
+#         try:
+#             goo = Good.objects.get(pk = id)
+#             s = "Товар (" + str(goo.pk) + "): " + goo.name + "<br>"
+#         except Good.DoesNotExist:
+#             s = "<b>Нет такого товара (" + str(id) + ")</b>" + "<br>"
+#     return HttpResponse(s)
 
 def good(request, id):
-    if id == None:
-        s = "Не выбран id товара"
-    else:
-        try:
-            goo = Good.objects.get(pk = id)
-            s = "Товар (" + str(goo.pk) + "): " + goo.name + "<br>"
-        except Good.DoesNotExist:
-            s = "<b>Нет такого товара (" + str(id) + ")</b>" + "<br>"
-    return HttpResponse(s)
+    try:
+        good = Good.objects.get(pk = id)
+    except Good.DoesNotExist:
+        return HttpResponse("<b>Нет такого товара (" + str(id) + ")</b>" + "<br>")
+    return render(request, "good.html", {"good": good})
