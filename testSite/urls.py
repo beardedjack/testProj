e6e86c3225342df8e6d2910e351e7e4007ad37b9
kondrashov <evgen.kondrashoff@gmail.com>
2016-06-16 11:45:24 +0400

    edit views

--- testSite/urls.py
+++ testSite/urls.py
@@ -1,8 +1,6 @@
 from django.conf.urls import patterns, url
-from testSite import views
-
-urlpatterns = patterns('',
-url(r'^(?:(?P<id>\d+)/)?$', views.index, name = "index"),
-url(r'^good/(?P<id>\d+)/$', views.good, name = "good"))
 
 
+urlpatterns = patterns('testSite.views',
+                       url(r'^(?:(?P<id>\d+)/)?$', 'index'),
+                       url(r'^good/(?P<id>\d+)/$', 'good'))
--- testSite/views.py
+++ testSite/views.py
@@ -5,18 +5,24 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from testSite.models import Category, Good
 
-def index(request, cat_id):
-
-
-    if cat_id is None:
+def index(request, id):
+    if id == None:
         cat = Category.objects.first()
     else:
-        cat = Category.objects.get(pk = cat_id)
+        cat = Category.objects.get(pk = id)
     goods = Good.objects.filter(category = cat).order_by("name")
     s = "Категория: " + cat.name + "<br><br>"
     for good in goods:
         s = s + "(" + str(good.pk) + ") " + good.name + "<br>"
     return HttpResponse(s)
 
-def good(request):
-    return ""
+def good(request, id):
+    if id == None:
+        s = "Не выбран id товара"
+    else:
+        try:
+            goo = Good.objects.get(pk = id)
+            s = "Товар (" + str(goo.pk) + "): " + goo.name + "<br>"
+        except Good.DoesNotExist:
+            s = "<b>Нет такого товара (" + str(id) + ")</b>" + "<br>"
+    return HttpResponse(s)
