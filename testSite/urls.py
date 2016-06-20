c0bc619063e29a1d5002b1e7c95204c2252bd0d8
kondrashov <evgen.kondrashoff@gmail.com>
2016-06-20 14:36:11 +0400

    before correct page

--- testSite/urls.py
+++ testSite/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import patterns, url
+from testSite import views
 
-
-urlpatterns = patterns('testSite.views',
-                       url(r'^(?:(?P<id>\d+)/)?$', 'index'),
-                       url(r'^good/(?P<id>\d+)/$', 'good'))
+urlpatterns = patterns('',
+                       url(r'^(?:(?P<id>\d+)/)?$', views.index, name="index"),
+                       url(r'^good/(?P<id>\d+)/$', views.good, name="good"))
--- testSite/views.py
+++ testSite/views.py
@@ -4,48 +4,49 @@ from django.shortcuts import render
 
 from django.http import HttpResponse
 from testSite.models import Category, Good
+from django.core.paginator import Paginator, InvalidPage
 
-
+# before paginator
 # def index(request, id):
+#     cats = Category.objects.all().order_by("name")
 #     if id == None:
 #         cat = Category.objects.first()
 #     else:
 #         cat = Category.objects.get(pk = id)
 #     goods = Good.objects.filter(category = cat).order_by("name")
-#     s = "Категория: " + cat.name + "<br><br>"
+#     pricesum = 0
 #     for good in goods:
-#         s = s + "(" + str(good.pk) + ") " + good.name + "<br>"
-#     return HttpResponse(s)
+#         pricesum = pricesum + good.price
+#     return render(request, "index.html", {"category": cat, "cats": cats, "goods": goods, "pricesum": pricesum})
 
 def index(request, id):
+    try:
+        page_num = request.GET["page"]
+    except KeyError:
+        page_num = 1
     cats = Category.objects.all().order_by("name")
     if id == None:
         cat = Category.objects.first()
     else:
         cat = Category.objects.get(pk = id)
-    goods = Good.objects.filter(category = cat).order_by("name")
+
+    paginator = Paginator(Good.objects.filter(category=cat).order_by("name"), 3)
+
+    try:
+        goods = paginator.page(page_num)
+    except InvalidPage:
+        goods = paginator.page(1)
+
+    allgoods = Good.objects.filter(category=cat).order_by("name")
     pricesum = 0
-    for good in goods:
+    for good in allgoods:
         pricesum = pricesum + good.price
-    return render(request, "index.html", {"category": cat, "cats": cats, "goods": goods, "pricesum": pricesum})
+    goodscount = allgoods.count
 
-# def good(request, id):
-#     if id == None:
-#         s = "Не выбран id товара"
-#     else:
-#         try:
-#             goo = Good.objects.get(pk = id)
-#             s = "Товар (" + str(goo.pk) + "): " + goo.name + "<br>"
-#         except Good.DoesNotExist:
-#             s = "<b>Нет такого товара (" + str(id) + ")</b>" + "<br>"
-#     return HttpResponse(s)
 
-# def good(request, id):
-#     try:
-#         good = Good.objects.get(pk = id)
-#     except Good.DoesNotExist:
-#         return HttpResponse("<b>Нет такого товара (" + str(id) + ")</b>" + "<br>")
-#     return render(request, "good.html", {"good": good})
+
+
+    return render(request, "index.html", {"category": cat, "cats": cats, "goods": goods, "goodscount":goodscount, "pricesum": pricesum})
 
 def good(request, id):
     cats = Category.objects.all().order_by("name")
