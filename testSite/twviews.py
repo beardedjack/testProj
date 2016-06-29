5a4cb702a43312d697bb6b579c56408bf72237f1
kondrashov <evgen.kondrashoff@gmail.com>
2016-06-29 16:39:07 +0400

    add new classes

--- testSite/twviews.py
+++ testSite/twviews.py
@@ -1,14 +1,14 @@
-from django.views.generic.base import TemplateView
-from django.core.paginator import Paginator, InvalidPage
 from testSite.models import Category, Good
-from django.http import Http404
-
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
-
 from django.views.generic.base import ContextMixin
 
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic.edit import ProcessFormView
+from django.core.urlresolvers import reverse
+
 class CategoryListMixin(ContextMixin):
+
     def get_context_data(self, **kwargs):
         context = super(CategoryListMixin, self).get_context_data(**kwargs)
         context["cats"] = Category.objects.order_by("name")
@@ -26,27 +26,13 @@ class GoodListView(ListView, CategoryListMixin):
             self.cat = Category.objects.get(pk = self.kwargs["id"])
         return super(GoodListView, self).get(request, *args, **kwargs)
 
-
     def get_context_data(self, **kwargs):
         context = super(GoodListView, self).get_context_data(**kwargs)
-        #context["cats"] = Category.objects.order_by("name")
         context["category"] = self.cat
         return context
 
     def get_queryset(self):
         return Good.objects.filter(category=self.cat).order_by("name")
-
-
-
-
-        # allgoods = Good.objects.filter(category=iid).order_by("name")
-        # context["goodscount"] = allgoods.count
-        # pricesum = 0
-        # for good in allgoods:
-        #     pricesum = pricesum + good.price
-        # context["pricesum"] = pricesum
-        # return context
-
 
 class GoodDetailView(DetailView, CategoryListMixin):
     template_name = "good.html"
@@ -59,22 +45,65 @@ class GoodDetailView(DetailView, CategoryListMixin):
             context["pn"] = self.request.GET["page"]
         except KeyError:
             context["pn"] = "1"
-        #context["cats"] = Category.objects.order_by("name")
         return context
 
-    #
-    # def get_context_data(self, **kwargs):
-    #     context = super(GoodDetailView, self).get_context_data(**kwargs)
-    #     try:
-    #         context["pn"] = self.request.GET["page"]
-    #     except KeyError:
-    #         context["pn"] = 1
-    #     context["cats"] = Category.objects.order_by("name")
-    #     try:
-    #         context["good"] = Good.objects.get(pk = kwargs["id"])
-    #     except Good.DoesNotExist:
-    #         raise Http404("Нет такого товара")
-    #
-    #     return context
+class GoodEditMixin(CategoryListMixin):
 
+    def get_context_data(self, **kwargs):
+        context = super(GoodEditMixin, self).get_context_data(**kwargs)
+        try:
+            context["pn"] = self.request.GET["page"]
+        except KeyError:
+            context["pn"] = "1"
+        return context
 
+class GoodEditView(ProcessFormView):
+
+    def post(self, request, *args, **kwargs):
+        try:
+            pn = request.GET["page"]
+        except KeyError:
+            pn = "1"
+        self.success_url = self.success_url + "?page=" + pn
+        return super(GoodEditView, self).post(request, *args, **kwargs)
+
+class GoodCreate(CreateView, GoodEditMixin):
+    model = Good
+    template_name = "good_add.html"
+
+    def get(self, request, *args, **kwargs):
+        if self.kwargs["id"] != None:
+            self.initial["category"] = Category.objects.get(pk = self.kwargs["id"])
+        return super(GoodCreate, self).get(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        self.success_url = reverse("index", kwargs = {"id": Category.objects.get(pk = self.kwargs["id"]).id})
+        return super(GoodCreate, self).post(request, *args, **kwargs)
+
+    def get_context_data(self, **kwargs):
+        context = super(GoodCreate, self).get_context_data(**kwargs)
+        context["category"] = Category.objects.get(pk = self.kwargs["id"])
+        return context
+
+class GoodUpdate(UpdateView, GoodEditMixin, GoodEditView):
+    model = Good
+    template_name = "good_edit.html"
+    pk_url_kwarg = "id"
+
+    def post(self, request, *args, **kwargs):
+        self.success_url = reverse("index", kwargs = {"id": Good.objects.get(pk = kwargs["id"]).category.id})
+        return super(GoodUpdate, self).post(request, *args, **kwargs)
+
+class GoodDelete(DeleteView, GoodEditMixin, GoodEditView):
+    model = Good
+    template_name = "good_delete.html"
+    pk_url_kwarg = "id"
+
+    def post(self, request, *args, **kwargs):
+        self.success_url = reverse("index", kwargs={"id": Good.objects.get(pk=kwargs["id"]).category.id})
+        return super(GoodDelete, self).post(request, *args, **kwargs)
+
+    def get_context_data(self, **kwargs):
+        context = super(GoodDelete, self).get_context_data(**kwargs)
+        context["good"] = Good.objects.get(pk=kwargs["id"])
+        return context
--- testSite/urls.py
+++ testSite/urls.py
@@ -1,13 +1,13 @@
 from django.conf.urls import patterns, url
-from testSite import views
 from testSite.twviews import GoodListView, GoodDetailView
 
-# urlpatterns = patterns('',
-#                        url(r'^(?:(?P<id>\d+)/)?$', views.index, name="index"),
-#                        url(r'^good/(?P<id>\d+)/$', views.good, name="good"))
-
+from testSite.twviews import GoodCreate, GoodUpdate, GoodDelete
 
 urlpatterns = patterns('',
-                        url(r'^(?:(?P<id>\d+)/)?$', GoodListView.as_view(), name="index"),
-                        url(r'^good/(?P<id>\d+)/$', GoodDetailView.as_view(), name="good"))
+    url(r'^(?:(?P<id>\d+)/)?$', GoodListView.as_view(), name="index"),
+    url(r'^good/(?P<id>\d+)/$', GoodDetailView.as_view(), name="good"),
+    url(r'^(?P<id>\d+)/add/$', GoodCreate.as_view(), name="good_add"),
+    url(r'^good/(?P<id>\d+)/edit/$', GoodUpdate.as_view(), name="good_edit"),
+    url(r'^good/(?P<id>\d+)/delete/$', GoodDelete.as_view(), name="good_delete"),
+)
 
